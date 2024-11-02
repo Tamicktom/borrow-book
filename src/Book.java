bd74458704448285541ef2926ff7788612e3f45c
Tamicktom <rickpokemon38@gmail.com>
2024-11-02 18:55:55 -0300

    create book class

--- src/Book.java
+++ src/Book.java
@@ -1,5 +1,19 @@
 package src;
 
+import java.util.Date;
+
 public class Book {
-  
+  int id;
+  String title;
+  boolean borrowed = false;
+  Author author;
+
+  Date dateAdded = new Date();
+  Date dateUpdated = new Date();
+
+  public Book(int id, String title, Author author) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+  }
 }
